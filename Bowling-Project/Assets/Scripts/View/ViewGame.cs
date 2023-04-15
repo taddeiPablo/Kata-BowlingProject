89d882a2fb1c26ace753d7a59fc50eb2437fbdda
Sebastian Zambrano <50499778+SebastianZambranoR@users.noreply.github.com>
2023-04-15 17:08:30 -0500

    ViewGame

---
+++ Bowling-Project/Assets/Scripts/View/ViewGame.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewGame : MonoBehaviour
+{
+    private TMP_InputField _inputField;
+
+    private Button _shootButton;
+    private ShootButton _shootButtonScript;
+
+    private void Start()
+    {
+        _shootButtonScript = _shootButton.GetComponent<ShootButton>();
+        _shootButtonScript.OnShoot += ValidateValues;
+
+    }
+
+    private void ValidateValues()
+    {
+        bool haveText = _inputField.text.Length > 0;
+        bool correctString = _inputField.text.Contains(",");
+
+        bool correctNumbers;
+        string[] values = _inputField.text.Split(',');
+
+        int[] numbers = new int[10];
+
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            int n;
+            correctNumbers = int.TryParse(values[i], out n);
+
+            if (correctNumbers)
+            {
+                numbers[i] = n;
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+        
+        
+        
+    }
+
+    private void OnDisable()
+    {
+        _shootButtonScript.OnShoot -= ValidateValues;
+    }
+}
