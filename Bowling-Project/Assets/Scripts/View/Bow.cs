d0e2954b12785d5660b72fcd8c98d1811919b5e7
Sebastian Zambrano <50499778+SebastianZambranoR@users.noreply.github.com>
2023-04-15 16:52:04 -0500

    Set UI

---
+++ Bowling-Project/Assets/Scripts/View/Bow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> bows;
+    // Start is called before the first frame update
+
+    
+    
+}
